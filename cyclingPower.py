bdd2b9d424fbd1c5abd682294b1f95d5f4f718fb
EzrSchwartz <146258152+EzrSchwartz@users.noreply.github.com>
2024-01-21 19:41:30 -0500

    Update cyclingPower.py

--- cyclingPower.py
+++ cyclingPower.py
@@ -1,4 +1,25 @@
+'''
+CircuitPython Library for extracting power from a power meter.
+This library can be used to extract the power value in watts 
+This was created by Ezra Schwartz
+It was tested with a Quarq Power Meter and was returning the same values that where found on the wahoo cycling computer that was connected to the power meter at the time of testing. 
 
+
+The power_Values function will return the power value that is sent in the packet
+
+The ByteArray function will return the raw data from the packet.
+
+
+Example Usage:
+
+        power_sensor = ble.connect(advertisement)
+
+        power_service = power_sensor[CyclingPowerService]
+        
+        print(power_service.power_Value)
+
+
+'''
 
 import struct
 from collections import namedtuple
@@ -6,12 +27,7 @@ import _bleio
 from adafruit_ble.services import Service
 from adafruit_ble.uuid import StandardUUID
 from adafruit_ble.characteristics import Characteristic, ComplexCharacteristic
-from adafruit_ble.characteristics.int import Uint8Characteristic
 
-prev_Tot_Time = 0
-prev_Tot_Power = 0
-Tot_power = 0
-Tot_time = 0
 try:
     from typing import Optional
 except ImportError:
@@ -20,7 +36,7 @@ except ImportError:
 CPMeasurementValues = namedtuple(
     "CPMeasurementValues",
     (
-        "hex"
+        "value"
     ),
 )
 
@@ -41,7 +57,6 @@ class CyclingPowerService(Service):
     """Service for reading from a Cycling Power sensor."""
     uuid = StandardUUID(0x1818)  
 
-
     cp_measurement = _CPMeasurement()
 
     def __init__(self, service: Optional["CyclingPowerService"] = None) -> None:
@@ -49,10 +64,33 @@ class CyclingPowerService(Service):
         self._measurement_buf = None
 
     @property
-    def power_meter_values(self) -> Optional[CPMeasurementValues]:
+    def power_Value(self) -> Optional[CPMeasurementValues]:  #returns the power value
+
         if self._measurement_buf is None:
             self._measurement_buf = bytearray(self.cp_measurement.incoming_packet_length)
 
+        # Clear the buffer
+        for i in range(len(self._measurement_buf)):
+            self._measurement_buf[i] = 0
+
+        packet_length = self.cp_measurement.readinto(self._measurement_buf)
+        if packet_length == 0:
+            return None
+#find the byte pair for power and return it
+        for i in range(0, len(self._measurement_buf), 2):
+            if i + 2 <= len(self._measurement_buf):
+                if(i == 2):
+                    value = struct.unpack_from('<H', self._measurement_buf, i)[0]
+                    return value
+                else:
+                    continue
+
+
+
+    @property
+    def byte_array(self) -> Optional[CPMeasurementValues]: #returns the raw Byte Array from the packet
+        if self._measurement_buf is None:
+            self._measurement_buf = bytearray(self.cp_measurement.incoming_packet_length)
 
     # Clear the buffer
         for i in range(len(self._measurement_buf)):
@@ -61,41 +99,12 @@ class CyclingPowerService(Service):
         packet_length = self.cp_measurement.readinto(self._measurement_buf)
         if packet_length == 0:
             return None
+#set Byte Array Value
+        ByteArray = ''.join('{:02x}'.format(x) for x in self._measurement_buf)
 
-        hex_data = ''.join('{:02x}'.format(x) for x in self._measurement_buf)
+        return ByteArray
         
-        byte_pairs = [hex_data[i:i+4] for i in range(0, len(hex_data), 4)]
-
-    # Printing each 2-byte pair and its corresponding decimal value
-        # for i, byte_pair in enumerate(byte_pairs):
-        #     decimal_value = int(byte_pair, 16)
-        #     if i == 2:
-        #         Tot_time == decimal_value
-        #         print(Tot_power)
-        #     if i == 3:
-        #         Tot_power == decimal_value
-        #         print(Tot_time)
-
-        # power = ((Tot_power-prev_Tot_Power)*1024)/((Tot_time-prev_Tot_Time)+1)
-        # print(power)   
-
-        # for i, byte_pair in enumerate(byte_pairs):
-        #     decimal_value = int(byte_pair, 16)
-        #     if i == 2:
-        #         Tot_time == decimal_value
-        #     if i == 3:
-        #         Tot_power == decimal_value
-        byte_data = bytes.fromhex(hex_data)
-
-        for i in range(0, len(byte_data), 2):
-            if i + 2 <= len(byte_data):
-                value = struct.unpack_from('<H', byte_data, i)[0]
-                print(f"Value at bytes {i}-{i+1}: {value}")
 
 
 
-        return hex_data
-
-
-#2c08(Label) 0000 950a f401 dbf2 fbde 0000 0000 0000 0000
 
